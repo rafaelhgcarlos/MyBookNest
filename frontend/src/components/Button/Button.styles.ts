0f059e9e57778afa0c56ba3557746c1500bc09d2
Rafael Gonzaga <rafael_hgcarlos@hotmail.com>
2025-08-11 21:26:54 -0300

    refactor: refatora estilos do botão

--- frontend/src/components/Button/Button.styles.ts
+++ frontend/src/components/Button/Button.styles.ts
@@ -1,44 +1,48 @@
-import type {ButtonSize, ButtonStyle, ButtonState} from "./Button.types.ts";
+import type { ButtonSize, ButtonStyle, ButtonState } from "./Button.types.ts";
 import clsx from "clsx";
 
 export default function getButtonStyle(
     size: ButtonSize,
     style: ButtonStyle,
     state: ButtonState
-){
-    const baseClasses = 'w-auto flex justify-center items-center transition-all rounded-md';
+) {
+    const baseClasses = "w-auto flex justify-center items-center transition-colors duration-300 rounded-md font-semibold select-none";
 
     const sizeClasses = {
-        sm:'px-3 py-2 gap-1 text-sm',
-        md:'px-3 py-2 gap-1 text-base',
-        lg:'px-4 py-4 gap-2 text-lg',
-        xl:'px-5 py-4 gap-2 text-2xl'
-    }
+        sm: "px-3 py-2 gap-1 text-sm",
+        md: "px-4 py-2 gap-1 text-base",
+        lg: "px-5 py-3 gap-2 text-lg",
+        xl: "px-6 py-4 gap-2 text-2xl",
+    };
 
     const iconSizeClasses = {
         sm: 14,
         md: 18,
         lg: 22,
         xl: 24,
-    }
+    };
 
     const styleClasses = {
-        primary: 'text-stone-50 bg-blue-500',
-        secondary: 'text-blue-500 border border-blue-500',
-    }
-
-    const hoverClasses = {
-        primary: 'hover:bg-blue-400',
-        secondary: 'hover:bg-blue-300/80',
-    }
+        primary:
+            "bg-blue-600 text-white hover:bg-blue-500 focus:outline-none focus:ring-4 focus:ring-blue-300 focus:ring-opacity-50 shadow-md",
+        secondary:
+            "bg-transparent text-blue-600 border border-blue-600 hover:bg-blue-100 focus:outline-none focus:ring-4 focus:ring-blue-300 focus:ring-opacity-50",
+        ghost:
+            "bg-transparent text-white hover:text-blue-400 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-50",
+    };
 
     const stateClasses = {
-        enabled: clsx('hover:cursor-pointer opacity-100', hoverClasses[style]),
-        disabled: 'cursor-not-allowed opacity-50'
-    }
+        enabled: clsx("cursor-pointer opacity-100"),
+        disabled: "cursor-not-allowed opacity-50 pointer-events-none",
+    };
 
-    const buttonStyle = clsx(baseClasses, sizeClasses[size], styleClasses[style], stateClasses[state]);
+    const buttonStyle = clsx(
+        baseClasses,
+        sizeClasses[size],
+        styleClasses[style],
+        stateClasses[state]
+    );
     const iconSize = iconSizeClasses[size];
 
-    return {buttonStyle, iconSize};
+    return { buttonStyle, iconSize };
 }
