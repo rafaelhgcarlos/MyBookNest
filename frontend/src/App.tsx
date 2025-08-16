113abd022ee92c01ab683b7167ed1bf45703daad
Rafael Gonzaga <rafael_hgcarlos@hotmail.com>
2025-08-16 08:28:54 -0300

    feat: adiciona pagina de notfound ao projeto

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home.tsx";
 import Library from "./pages/Library.tsx";
+import NotFound from "./pages/NotFound.tsx";
 
 export default function App() {
 
@@ -9,6 +10,7 @@ export default function App() {
           <Routes>
               <Route path="/" element={<Home />} />
               <Route path="/biblioteca" element={<Library />} />
+              <Route path="*" element={<NotFound/>}/>
           </Routes>
       </Router>
   )
